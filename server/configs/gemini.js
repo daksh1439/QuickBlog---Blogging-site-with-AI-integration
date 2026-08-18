e973432e4bfe4540867b2408634d7d4083dd2bac
daksh1439 <dakshgondi06@gmail.com>
2026-08-18 20:14:25 +0530

    Added project files

---
+++ server/configs/gemini.js
@@ -0,0 +1,22 @@
+import OpenAI from "openai";
+
+const client = new OpenAI({
+  apiKey: process.env.GROQ_API_KEY,
+  baseURL: "https://api.groq.com/openai/v1",
+});
+
+async function main(prompt) {
+  const response = await client.chat.completions.create({
+    model: "llama-3.3-70b-versatile",
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  });
+
+  return response.choices[0].message.content;
+}
+
+export default main;
